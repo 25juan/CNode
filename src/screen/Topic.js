4612886b499fdd460175f61b6f43ce5d5af9b9ea
i5juan <1216181067@qq.com>
2018-02-01 22:16:56 +0800

    主题详情画面刷新功能完成

--- src/screen/Topic.js
+++ src/screen/Topic.js
@@ -19,10 +19,12 @@ import {
 } from "mobx-react/native";
 import { observable } from "mobx" ;
 import alert from "../component/Alert" ;
-import Menu, { MenuItem, MenuDivider } from 'react-native-material-menu';
+import Menu, { MenuItem } from 'react-native-material-menu';
+import Toast from "react-native-easy-toast" ;
 @inject("common")
 @observer
 export default class Topic extends Component{
+    toast = null;
     @observable
     topic = {};
     static navigationOptions={header:null} ;
@@ -30,6 +32,9 @@ export default class Topic extends Component{
         this.menu = ref;
     };
     refresh(){
+        let { article } = this.props.navigation.state.params;
+        this.topic = this.parse2Article(article) ;
+        this.toast.show("刷新成功！");
         this.menu.hide();
     }
     share(){
@@ -58,27 +63,24 @@ export default class Topic extends Component{
     back(){
         this.props.navigation.goBack();
     }
-    componentWillReact(){
-        /*let { article } = this.props.navigation.state.params;
+    parse2Article(article={}){
         let topic = {} ;
         topic.content = article.content || "";
         topic.title = article.title || "" ;
         topic.visit =article["visit_count"] || "" ;
         topic.create = moment(article["create_at"]).format("YYYY-MM-DD") || " " ;
-        this.topic = topic ;
-        console.log(this.topic);*/
-        console.log("render...")
+        topic.author = article.author || {} ;
+        return topic ;
+    }
+    componentWillMount(){
+        let { article } = this.props.navigation.state.params;
+        this.topic = this.parse2Article(article) ;
     }
     render(){
-        let { article } = this.props.navigation.state.params;
         let { theme } = this.props.common ;
-        let topic = {} ;
-        topic.content = article.content || "";
-        topic.title = article.title || "" ;
-        topic.visit =article["visit_count"] || "" ;
-        topic.create = moment(article["create_at"]).format("YYYY-MM-DD") || " " ;
+        let topic = this.topic ;
         return (<Col>
-                    <Header title={article.author.loginname} onPress={()=>this.back()} theme={theme}>
+                    <Header title={topic.author.loginname} onPress={()=>this.back()} theme={theme}>
                         <Right>
                             <Button onPress={()=>this.share()} transparent>
                                 <Icon style={{color:theme.headerTextColor}} android={"md-share-alt"} ios={"ios-share-alt-outline"}/>
@@ -98,6 +100,7 @@ export default class Topic extends Component{
                         </Right>
                     </Header>
                     <SuperWebView theme={theme} topic={topic}/>
+                    <Toast ref={(toast)=>this.toast = toast}/>
                 </Col>)
     }
 }

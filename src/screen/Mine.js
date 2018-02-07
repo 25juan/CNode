c1286d72dbae296da1498c3cbaf8f9162681942d
25juan <1216181067@qq.com>
2018-02-07 18:09:29 +0800

    修改我的面板列表

--- src/screen/Mine.js
+++ src/screen/Mine.js
@@ -1,4 +1,4 @@
-import React,{
+import React, {
     Component
 } from "react" ;
 import {
@@ -7,8 +7,8 @@ import {
     ScrollView, Linking
 } from "react-native";
 import {
-    Card,Body,Container,Content,
-    Row,Col,Thumbnail,Right,Icon,CardItem,Text,Button
+    Card, Body, Container, Content,
+    Row, Col, Thumbnail, Right, Icon, CardItem, Text, Button
 } from "native-base";
 import Touchable from "../component/Touchable" ;
 import {
@@ -17,84 +17,108 @@ import {
 } from "mobx-react/native";
 import StatusBar from "../component/StatusBar" ;
 import Toast from "react-native-easy-toast" ;
+
 @inject("common")
 @inject("user")
 @observer
-export default class Mine extends Component{
-    toast = null ;
-    openSkin(){
-        let { navigate } = this.props.navigation ;
+export default class Mine extends Component {
+    toast = null;
+
+    openSkin() {
+        let {navigate} = this.props.navigation;
         navigate("SkinPicker");
     }
-    login(){}
-    clearCache(){
-        this.toast.show("缓存清除成功") ;
+
+    login() {
     }
-    openCNode(){
+
+    clearCache() {
+        this.toast.show("缓存清除成功");
+    }
+
+    openCNode() {
         Linking.openURL("https://cnodejs.org/about");
     }
-    openSoftware(){
+
+    openSoftware() {
         Linking.openURL("https://github.com/25juan/CNode");
     }
-    render(){
-        let theme = this.props.common.theme ;
-        let { loginname , avatar_url  } = this.props.user.user ;
+
+    render() {
+        let theme = this.props.common.theme;
+        let {loginname, avatar_url} = this.props.user.user;
         return (
             <Container style={styles.white}>
                 <StatusBar theme={theme}/>
-                <ScrollView>
-                    <Content padder>
-                        <Card>
-                            <Touchable onPress={()=>this.login()}>
-                                <CardItem>
-                                    <Body style={styles.row}>
-                                    <View style={styles.center}>
-                                        <Text style={styles.loginText}>{ loginname }</Text>
-                                    </View>
-                                    <View style={styles.center}>
-                                        <Thumbnail source={avatar_url}/>
-                                    </View>
-                                    </Body>
-                                </CardItem>
-                            </Touchable>
-                        </Card>
-                        <Card>
-                            <CardItem style={styles.borderBottom}><Text>我的文章</Text></CardItem>
-                            <CardItem style={styles.borderBottom}><Text>我的收藏</Text></CardItem>
-                            <CardItem><Text>消息通知</Text></CardItem>
-                        </Card>
-                        <Card>
-                            <Touchable onPress={()=>this.clearCache()}>
-                                <CardItem style={styles.borderBottom}><Text>清除缓存</Text></CardItem>
-                            </Touchable>
-                            <Touchable onPress={()=>this.openCNode()}>
-                                <CardItem style={styles.borderBottom}><Text>关于CNode</Text></CardItem>
-                            </Touchable>
-                            <Touchable onPress={()=>this.openSoftware()}>
-                                <CardItem style={styles.borderBottom}><Text>关于本软件</Text></CardItem>
-                            </Touchable>
-                            <Touchable onPress={()=>this.openSkin()}>
-                                <CardItem>
-                                    <Text >更换皮肤</Text>
-                                </CardItem>
-                            </Touchable>
-                        </Card>
-                        <View>
-                            <Button block danger>
-                                <Text>退出登录</Text>
-                            </Button>
-                        </View>
-                    </Content>
-                </ScrollView>
-                <Toast ref={(toast)=>this.toast = toast}/>
+                <Content padder>
+                    <Card>
+                        <Touchable onPress={() => this.login()}>
+                            <CardItem>
+                                <Body style={styles.row}>
+                                <View style={styles.center}>
+                                    <Text style={styles.loginText}>{loginname}</Text>
+                                </View>
+                                <View style={styles.center}>
+                                    <Thumbnail source={avatar_url}/>
+                                </View>
+                                </Body>
+                            </CardItem>
+                        </Touchable>
+                    </Card>
+                    <Card>
+                        <CardItem style={styles.borderBottom}><Text>我的文章</Text></CardItem>
+                        <CardItem style={styles.borderBottom}><Text>我的收藏</Text></CardItem>
+                        <CardItem><Text>消息通知</Text></CardItem>
+                    </Card>
+                    <Card>
+                        <Touchable onPress={() => this.clearCache()}>
+                            <CardItem style={styles.borderBottom}>
+                                <Body><Text>清除缓存</Text></Body>
+                                <Right>
+                                    <Icon name="ios-arrow-forward-outline"/>
+                                </Right>
+                            </CardItem>
+                        </Touchable>
+                        <Touchable onPress={() => this.openCNode()}>
+                            <CardItem style={styles.borderBottom}>
+                                <Body><Text>关于CNode</Text></Body>
+                                <Right>
+                                    <Icon name="ios-arrow-forward-outline"/>
+                                </Right>
+                            </CardItem>
+                        </Touchable>
+                        <Touchable onPress={() => this.openSoftware()}>
+                            <CardItem style={styles.borderBottom}>
+                                <Body><Text>关于本软件</Text></Body>
+                                <Right>
+                                    <Icon name="ios-arrow-forward-outline"/>
+                                </Right>
+                            </CardItem>
+                        </Touchable>
+                        <Touchable onPress={() => this.openSkin()}>
+                            <CardItem style={styles.borderBottom}>
+                                <Body><Text>更换皮肤</Text></Body>
+                                <Right>
+                                    <Icon name={"ios-arrow-forward-outline"}/>
+                                </Right>
+                            </CardItem>
+                        </Touchable>
+                    </Card>
+                    <View>
+                        <Button block danger>
+                            <Text>退出登录</Text>
+                        </Button>
+                    </View>
+                </Content>
+                <Toast ref={(toast) => this.toast = toast}/>
             </Container>
         )
     }
 }
-const styles= {
-    white:{backgroundColor:"#fff"},
-    row:{flexDirection:"row"},
-    borderBottom:{borderBottomWidth:1,borderBottomColor:"#ededed"},
-    center:{flex:1,alignItems:"center",justifyContent:"center",height:80},
-    loginText:{fontSize:24}
+const styles = {
+    white: {backgroundColor: "#fff"},
+    row: {flexDirection: "row"},
+    borderBottom: {borderBottomWidth: 1, borderBottomColor: "#ededed"},
+    center: {flex: 1, alignItems: "center", justifyContent: "center", height: 80},
+    loginText: {fontSize: 24}
 };

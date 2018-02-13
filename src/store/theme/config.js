803a1ae2331c741b90bfa5af1960973403325bc8
25juan <1216181067@qq.com>
2018-02-13 10:11:09 +0800

    修改主题生成配置

---
+++ src/store/theme/config.js
@@ -0,0 +1,46 @@
+/***
+ * 该方法是通过传入一颜色值，生成主题皮肤
+ * @param color
+ */
+import Color from "color";
+export default (color) =>{
+    const MAINCOLOR = color ;
+    const COLOR = Color(MAINCOLOR) ;
+    const FabColor = COLOR.darken(0.3) ;
+    const loadingBackgroundColor = MAINCOLOR ;
+    const rcColor = MAINCOLOR ; //单复选框的颜色
+    const activeTintColor = MAINCOLOR ;
+    const headerBackgroundColor = MAINCOLOR ;
+    const statusBarColor = COLOR.darken(0.3) ;
+    const switchCircleColor = "#eee" ;
+    const tabBackgroundColor = MAINCOLOR ;
+    const inActiveTabTextColor = "#333";
+    const activeTabTextColor = "#fff" ;
+    const touchableBackgroundColor = COLOR.lighten(0.7) ;
+    const tabBarUnderlineColor = COLOR.darken(0.4) ;
+    const iosBarStyle = COLOR.isDark()?"light-content":"dark-content"; //light-content or
+    const inactiveTintColor = "#333";
+    const headerTextColor = "#fff";
+    const loadingColor = "#fff" ;
+    const listViewBackgroundColor = "#fff" ;
+    return {
+        MAINCOLOR,
+        touchableBackgroundColor,
+        listViewBackgroundColor,
+        inActiveTabTextColor,
+        activeTabTextColor,
+        tabBackgroundColor,
+        FabColor,
+        switchCircleColor,
+        activeTintColor,
+        inactiveTintColor,
+        headerBackgroundColor,
+        headerTextColor,
+        iosBarStyle,
+        loadingColor,
+        loadingBackgroundColor,
+        rcColor,
+        statusBarColor,
+        tabBarUnderlineColor
+    }
+};
--- src/store/theme/index.js
+++ src/store/theme/index.js
@@ -1,8 +1,9 @@
-import normal from "./normal" ;
-import dark from "./dark" ;
-import red from "./red" ;
 import markdown_light from "./markdow_light" ;
 import markdown_dark from "./markdown_dark" ;
+import config from "./config" ;
+const red =  config("red");
+const normal = config("#37C6C0") ;
+const dark = config("#333") ;
 export default {
     normal,
     dark,
